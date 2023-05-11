f66cf2e6bd16ae4be8d75f2d49347386c87ced56
HipJaengYiCat <shee6185@gmail.com>
2023-05-11 09:54:08 +0000

    [Add] exp code

--- mmdetection/tools/train.sh
+++ mmdetection/tools/train.sh
@@ -1,2 +1,2 @@
-python3 main.py --model cascade_rcnn_x101_64x4d_fpn_1x_coco --folder cascade_rcnn
-python3 main.py --model universenet101_2008d_fp16_4x4_mstrain_480_960_20e_coco --folder universenet
+python3 main.py --model tood_r101_fpn_dconv_c3-c5_mstrain_2x_coco --folder tood
+python3 main.py --model retinanet_pvtv2-b4_fpn_1x_coco --folder pvt
