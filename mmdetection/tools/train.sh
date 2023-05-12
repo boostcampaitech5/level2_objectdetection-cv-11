7b419925c84bb7eb5e4fa71e7b05a85bae9fdf07
HipJaengYiCat <shee6185@gmail.com>
2023-05-12 01:50:51 +0000

    [Docs] exp code fix

--- mmdetection/tools/train.sh
+++ mmdetection/tools/train.sh
@@ -1,2 +1,4 @@
 python3 main.py --model tood_r101_fpn_dconv_c3-c5_mstrain_2x_coco --folder tood
 python3 main.py --model retinanet_pvtv2-b4_fpn_1x_coco --folder pvt
+python3 main.py --model cascade_rcnn_s101_fpn_syncbn-backbone+head_mstrain-range_1x_coco --folder resnest
+
