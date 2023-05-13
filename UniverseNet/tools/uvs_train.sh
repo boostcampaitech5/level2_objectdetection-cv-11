b8ee8978b9c2f45c23ce3409ff7333fc3d546314
HipJaengYiCat <shee6185@gmail.com>
2023-05-13 16:45:29 +0000

    [Docs] exp -5.14, glfv2- swin2 & pvtv2(resize up)

--- UniverseNet/tools/uvs_train.sh
+++ UniverseNet/tools/uvs_train.sh
@@ -1,2 +1,5 @@
-python3 main.py --model gfl_pvt_v2_b2_fpn_fp16_detraug_3x_coco --folder pvtv2_original --resize 768
-python3 main.py --model gflv2_pvt_v2_b2_fpn --folder gflv2 --resize 768
+python3 main.py --model glfv2_swinv2-t-p4-w16_fpn_fp16_4x4_1x_coco_weight --folder swinv2 --resize 768
+python3 main.py --model glfv2_swinv2-t-p4-w16_fpn_fp16_4x4_1x_coco --folder swinv2 --resize 768
+python3 main.py --model gflv2_pvt_v2_b2_fpn --folder gflv2 --resize 900
+python3 main.py --model glfv2_swinv2-t-p4-w16_fpn_fp16_4x4_1x_coco_weight --folder swinv2
+python3 main.py --model glfv2_swinv2-t-p4-w16_fpn_fp16_4x4_1x_coco --folder swinv2
