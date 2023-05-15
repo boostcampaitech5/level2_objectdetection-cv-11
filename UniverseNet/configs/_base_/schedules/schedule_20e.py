f8666f87912f9e1deaed6202d8e2cf9de7c84c65
JaiyoungJoo <wodudwn@daum.net>
2023-05-15 01:30:44 +0000

    feat: add AdamW

--- UniverseNet/configs/_base_/schedules/schedule_20e.py
+++ UniverseNet/configs/_base_/schedules/schedule_20e.py
@@ -9,3 +9,20 @@ lr_config = dict(
     warmup_ratio=0.001,
     step=[16, 19])
 runner = dict(type='EpochBasedRunner', max_epochs=20)
+
+# optimizer = dict(
+#     _delete_=True,
+#     type='AdamW',
+#     lr=0.0001,
+#     betas=(0.9, 0.999),
+#     weight_decay=0.05,
+#     paramwise_cfg=dict(
+#         custom_keys={
+#             'absolute_pos_embed': dict(decay_mult=0.),
+#             'relative_position_bias_table': dict(decay_mult=0.),
+#             'norm': dict(decay_mult=0.)
+#         }))
+# lr_config = dict(step=[27, 33])
+# runner = dict(type='EpochBasedRunner', max_epochs=36)
+
+# fp16 = dict(loss_scale=dict(init_scale=512))
