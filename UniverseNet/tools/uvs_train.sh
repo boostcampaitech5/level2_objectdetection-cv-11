75e1d5a3fc6c25a5b7edadcf4294270d4e3949f6
HipJaengYiCat <shee6185@gmail.com>
2023-05-16 12:05:18 +0000

    [Refactory] exp code fix

--- UniverseNet/tools/uvs_train.sh
+++ UniverseNet/tools/uvs_train.sh
@@ -1,5 +1,5 @@
-python3 main.py --model glfv2_swinv2-t-p4-w16_fpn_fp16_4x4_1x_coco_weight --folder swinv2 --resize 768
-python3 main.py --model glfv2_swinv2-t-p4-w16_fpn_fp16_4x4_1x_coco --folder swinv2 --resize 768
-python3 main.py --model gflv2_pvt_v2_b2_fpn --folder gflv2 --resize 900
-python3 main.py --model glfv2_swinv2-t-p4-w16_fpn_fp16_4x4_1x_coco_weight --folder swinv2
-python3 main.py --model glfv2_swinv2-t-p4-w16_fpn_fp16_4x4_1x_coco --folder swinv2
+python3 main.py --model glfv2_swinv2-t-p4-w16_fpn_weight_sgd --folder swinv2 --resize 768
+python3 main_dyhead.py --model gflv2_pvt_v2_b2_fpn_sepc --folder gflv2 --resize 768
+python3 main.py --model gflv2_pvt_v2_b2_fpn_sepc --folder gflv2 --resize 768
+python3 main.py --model gflv2_pvt_v2_b2_fpn_dyhead --folder gflv2 --resize 768
+python3 main.py --model gflv2_pvt_v2_b2_fpn_albu --folder gflv2
